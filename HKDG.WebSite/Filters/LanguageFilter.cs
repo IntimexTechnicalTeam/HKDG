78e684dce3461f4a8d3646bac7cbe650d1f158ad
spider <spider@ptx.hk>
2022-12-08 14:22:21 +0800

    临时蝗交

--- HKDG.WebSite/Filters/LanguageFilter.cs
+++ HKDG.WebSite/Filters/LanguageFilter.cs
@@ -10,27 +10,6 @@ namespace HKDG.WebSite
     public class LanguageFilter : ActionFilterAttribute, IResultFilter
     {
 
-
-        //public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
-        //{
-        //    var token = context.HttpContext.Request.Cookies["access_token"];
-        //    string langCode = "C";
-        //    if (!token.IsEmpty())
-        //    {
-        //        var user =await RedisHelper.HGetAsync<CurrentUser>($"{CacheKey.CurrentUser}", token);
-        //        if (user != null) langCode = user.Language.ToString();
-        //    }
-        //    if (context.Result is ViewResult)
-        //    {
-
-        //        //string langCode = ((ViewResult)context.Result).ViewData["Lang"]?.ToString() ?? "C";
-        //        //((ViewResult)context.Result).ViewData["Lang"] = langCode;
-        //        //string cultureName = CultureHelper.GetSupportCulture(langCode);
-        //        //Thread.CurrentThread.CurrentCulture = new CultureInfo(cultureName);
-        //        //Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
-        //    }
-        //}
-
         public override void OnResultExecuted(ResultExecutedContext context)
         {
             var token = context.HttpContext.Request.Cookies["access_token"];
